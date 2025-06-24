db2fa4208478a144edaad9f173872b9df296d886
TDUG <49434383+TDUG01@users.noreply.github.com>
2025-06-24 18:49:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
 import gradio as gr
+import huggingface_hub
+import torch
 from pythainlp.tokenize import sent_tokenize
 from deep_translator import GoogleTranslator
 from dotenv import load_dotenv
 from diffusers import DiffusionPipeline
-import torch
+
 
 # setup
 load_dotenv()
-pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-3.5", torch_dtype = torch.float16, variant = "fp16")
+api_key = os.getenv("API_KEY")
+huggingface_hub.login(token=api_key)
+
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype = torch.float16, variant = "fp16")
 pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -16,3 +21,5 @@ if __name__ == "__main__":
     prompt = "A fantasy princess sitting in a golden castle, highly detailed, magical atmosphere"
     image = pipe(prompt=prompt).images[0]
     image.save("princess_sd35.png")
+    
+    print("done")

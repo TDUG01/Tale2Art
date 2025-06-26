ec82805a454caf950282469ccf54629a50c12480
TDUG <49434383+TDUG01@users.noreply.github.com>
2025-06-26 20:55:53 +0700

    Text Segmentation&Translate Test

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import torch
 from pythainlp.tokenize import sent_tokenize
 from deep_translator import GoogleTranslator
 from dotenv import load_dotenv
-from diffusers import StableDiffusionPipeline
+from diffusers import DiffusionPipeline
+from transformers import MarianMTModel, MarianTokenizer
 
 
 # setup
@@ -13,19 +14,35 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 huggingface_hub.login(token=api_key)
 
-text = """
-ในแม่น้ำสายหนึ่งมีจระเข้ชุกชุมถึงสามสายพันธุ์ด้วยกัน จึงทำให้ไม่มีใครกล้ามาจับปลา มีเพียงตาอยู่คนเดียวเท่านั้นที่คลุกคลีกับจระเข้และจับปลามาขายได้ เมื่อชาวบ้านเดือดร้อนที่ใช้แม่น้ำหล่อเลี้ยงชีวิตไม่ได้ เรื่องนี้จึงร้อนถึงหูพระราชา ตาอยู่จึงได้บอกกับพระราชาไปว่า ได้เลี้ยงจระเข้ตัวหนึ่งตั้งแต่ยังเล็กมันจึงไม่ทำร้าย ส่วนจระเข้ตัวอื่นถ้ามันกินอิ่มมันก็จะไม่ทำร้ายคน
+model_name = "Helsinki-NLP/opus-mt-th-en"
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
 
-พระราชาจึงได้มีพระราชโองการสั่งให้เสมียนไปนับจำนวนจระเข้เพื่อที่จะได้นำอาหารไปเลี้ยงพวกมันได้อย่างทั่วถึง เสมียนทั้งสามคนก็พยายามนับจระเข้ที่อยู่ทั้งบนบกและในน้ำ สุดท้ายก็นับจระเข้ได้คนละหนึ่งพันตัว รวมทั้งหมดมีจระเข้ถึงสามพันตัว และพระราชาก็ได้สั่งให้เลี้ยงอาหารจระเข้จนอิ่มและไม่ออกมาทำร้ายชาวบ้าน และหากินในแม่น้ำแห่งนี้ได้อย่างมีความสุข นิทานเรื่องนี้เป็นตำนานหรือนิทานพื้นบ้านของจังหวัดสุพรรณบุรี จนกลายมาเป็นชื่อตำบลจระเข้สามพันจนถึงทุกวันนี้
-"""
+ntext =  """ในแม่น้ำสายหนึ่งมีจระเข้ชุกชุมถึงสามสายพันธุ์ด้วยกัน จึงทำให้ไม่มีใครกล้ามาจับปลา มีเพียงตาอยู่คนเดียวเท่านั้นที่คลุกคลีกับจระเข้และจับปลามาขายได้ เมื่อชาวบ้านเดือดร้อนที่ใช้แม่น้ำหล่อเลี้ยงชีวิตไม่ได้ เรื่องนี้จึงร้อนถึงหูพระราชา ตาอยู่จึงได้บอกกับพระราชาไปว่า ได้เลี้ยงจระเข้ตัวหนึ่งตั้งแต่ยังเล็กมันจึงไม่ทำร้าย ส่วนจระเข้ตัวอื่นถ้ามันกินอิ่มมันก็จะไม่ทำร้ายคน
 
-pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-3.5-medium")
-pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
+พระราชาจึงได้มีพระราชโองการสั่งให้เสมียนไปนับจำนวนจระเข้เพื่อที่จะได้นำอาหารไปเลี้ยงพวกมันได้อย่างทั่วถึง เสมียนทั้งสามคนก็พยายามนับจระเข้ที่อยู่ทั้งบนบกและในน้ำ สุดท้ายก็นับจระเข้ได้คนละหนึ่งพันตัว รวมทั้งหมดมีจระเข้ถึงสามพันตัว และพระราชาก็ได้สั่งให้เลี้ยงอาหารจระเข้จนอิ่มและไม่ออกมาทำร้ายชาวบ้าน และหากินในแม่น้ำแห่งนี้ได้อย่างมีความสุข นิทานเรื่องนี้เป็นตำนานหรือนิทานพื้นบ้านของจังหวัดสุพรรณบุรี จนกลายมาเป็นชื่อตำบลจระเข้สามพันจนถึงทุกวันนี้"""
+
+#pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev")
+#pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
+
 
 
 if __name__ == "__main__":
+    print(ntext)
+    
+    lists = sent_tokenize(ntext)
+    for i in lists:
+        print(repr(i))
+        
+        AAinputs = tokenizer(i, return_tensors="pt", padding=True, truncation=True)
+        outputs = model.generate(**AAinputs)
+        print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+        print(GoogleTranslator(input='auto', output='en').translate(i))
+    
+    """
     prompt = "A fantasy princess sitting in a golden castle, highly detailed, magical atmosphere"
-    image = pipe(prompt=prompt, height=384, width=384).images[0]
+    image = pipe(prompt=prompt, height=512, width=512).images[0]
     image.save("princess_sd35.png")
     
-    print("done")
+    print("done") 
+    """
